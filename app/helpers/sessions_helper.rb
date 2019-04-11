b6378c1c3801cc6f254e67d363d7b8477cd93a55
Minato <minaton.web030720@gmail.com>
2019-04-12 03:53:21 +0900

    テストエラー修正

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,38 +1,46 @@
 module SessionsHelper
-
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
-
-  # ユーザーのセッションを永続的にする
+  
+  # ユーザーを永続的に復元できるようになった
   def remember(user)
-    user.remember
+    user.remember # => DB: remember_digest
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-
-# 記憶トークンcookieに対応するユーザーを返す
-def current_user
+  
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
+  def current_user
     if (user_id = session[:user_id])
-        @current_user ||= User.find_by(id: user_id)
+      @current_user ||= User.find_by(id: user_id)
+
     elsif (user_id = cookies.signed[:user_id])
-        user = User.find_by(id: user_id)
-        if user && user.authenticated?(cookies[:remember_token])
+      # raise
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
-        end
+      end
     end
-    end
+  end
 
-  # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
   end
-
-  # 現在のユーザーをログアウトする
+  
+   # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
+  
+  
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,10 +1,9 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-
-    # def setup
-    #   @user = users(:michael)
-    # end
+  def setup
+    @user = users(:michael)
+  end
 
   test "login with invalid information" do
     get login_path
@@ -15,19 +14,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
-
-  test "login with valid information" do
-    get login_path
-    post login_path, params: { session: { email:    @user.email,
-                                          password: 'password' } }
-    assert_redirected_to @user
-    follow_redirect!
-    assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
-  end
-
+  
   test "login with valid information followed by logout" do
     get login_path
     post login_path, params: { session: { email:    @user.email,
@@ -39,6 +26,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
@@ -48,8 +36,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+    
   end
-
+  
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
     assert_not_empty cookies['remember_token']
